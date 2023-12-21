a84bd1cee1d2ef39872f34e929b35423f8125fbd
nOSkr <122243978+nOSkr04@users.noreply.github.com>
2023-12-21 23:18:51 +0800

    sync

--- src/pages/privacy.js
+++ src/pages/privacy.js
@@ -3,39 +3,40 @@ import React from "react"
 const Privacy = () => {
   return (
     <div className="container">
-      <h1 className="title mb-5 mt-5">Үйлчилгээний нөхцөл</h1>
+      <h1 className="title mb-5 mt-5">Privacy policy</h1>
       <div className="blog__details-left">
-        <h3 className="mb-20">Товч үйлчилгээний нөхцөл</h3>
-        <h5>1. Ерөнхий</h5>
+        <h3 className="mb-20">Brief Terms of Service</h3>
+        <h5>1. General</h5>
         <li className="ml-50 mt-3">
-          1. Santa.mn-р дамжуулан өндөг олгох, авах, түүнийг ашиглах,
-          хэрэглэхтэй холбоотой үүсэх харилцааг зохицуулахад оршино.
+          1. Giving, receiving and using eggs through Santa.mn, is to manage the
+          relationships that arise related to use.
         </li>
         <li className="ml-50 mt-3">
-          2. Энэхүү үйлчилгээний товч нөхцлийг дээрх үйлчилгээг авахаас өмнө
-          хэрэглэгч хүлээн зөвшөөрч баталгаажуулсны үндсэн дээр хэрэгжинэ.
+          2. Before availing the service of this brief terms of service will be
+          implemented based on user acceptance and confirmation.
         </li>
         <li className="ml-50 mt-3">
-          3. Santa.mn болон энэхүү өндөгын үйл ажиллагаатай холбоотой мэдээ
-          мэдээлэл, контент, нийтлэл, хичээлийг зөвшөөрөлгүй түгээх, хуулбарлах
-          бүх үйлдлийг хориглоно.
+          3. News related to Santa.mn and this egg operation unauthorized
+          distribution or reproduction of information, content, articles or
+          courses all actions are prohibited.
         </li>
         <h5 className="mt-5">2. Бүртгэл</h5>
         <li className="ml-50 mt-3">
-          1. өндөг хамрагдахын тулд хэрэглэгч бүртгэл үүсгэсэн байна. Ингэхдээ
-          нэвтрэх “Нэр” болон “Нууц үгийг” өөрт тохируулан үүсгэж үүнийгээ
-          мартахгүй байх үүргийг хүлээнэ.
+          1. In order to participate in the egg, the user must create an
+          account. In doing so Create your own login “Name” and “Password”.
+          undertake not to forget.
         </li>
         <li className="ml-50 mt-3">
-          2. Хэрэглэгчийн мэдээллийн нууцлалыг Santa.mn хамгаална, задруулахгүй.
+          2. Santa.mn will protect the confidentiality of user information and
+          will not disclose it.
         </li>
         <h5 className="mt-5">3. Төлбөр</h5>
         <li className="ml-50 mt-3">
-          1. Хэрэглэгч заагдсан үнийн дүнгээр төлбөрөө төлнө.
+          1. The user shall pay the amount indicated.
         </li>
         <li className="ml-50 mt-3">
-          2. Хэрэглэгч төлбөрөө төлөхөөс өмнө шийдвэрээ бүрэн зөв гаргасан байх
-          ба төлсний дараа төлбөр буцаагдахгүй.
+          2. The customer must have made a correct decision before paying and
+          once paid is non-refundable.
         </li>
       </div>
       <p>&nbsp;</p>
